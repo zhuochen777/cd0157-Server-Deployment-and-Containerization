17f34b59e279599ba6494bd979fb2e994c6aa9b9
zhuochen <zhuochen777@gmail.com>
2023-01-06 16:42:40 -0800

    add tests to the build

--- test_main.py
+++ test_main.py
@@ -31,10 +31,11 @@ def test_health(client):
 def test_auth(client):
     body = {'email': EMAIL,
             'password': PASSWORD}
-    response = client.post('/auth', 
+    response = client.post('/auth',
                            data=json.dumps(body),
                            content_type='application/json')
 
     assert response.status_code == 200
     token = response.json['token']
     assert token is not None
+    assert False
